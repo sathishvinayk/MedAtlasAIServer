b434c5e24a1005c4845b28b3c4aa865216f11f91
sathish vinayk <sathishvinayk@gmail.com>
2025-09-19 10:36:02 +0530

    generate a synthenzier done

--- scripts/curl.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST http://localhost:8000/process-audio \
-  -H "Content-Type: application/json" \
-  -d "{
-    \"audio_data\": \"$(base64 -i doctor_patient_conversation.mp3 | tr -d '\n')\",
-    \"file_name\": \"doctor_patient_conversation.mp3\"
-  }"
